fee809a6b35ecedabd3bb5edb252e8000bd71072
Joe Corall <jjc223@lehigh.edu>
2026-01-21 12:42:33 -0500

    Update travis_setup_drupal.sh

--- travis_setup_drupal.sh
+++ travis_setup_drupal.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 set -eou pipefail
-
 set -x
+
+export COMPOSER_MEMORY_LIMIT=-1
 
 echo "Setup database for Drupal"
 mysql -h 127.0.0.1 -P 3306 -u root -e "CREATE USER 'drupal'@'%' IDENTIFIED BY 'drupal';"
 mysql -h 127.0.0.1 -P 3306 -u root -e "GRANT ALL PRIVILEGES ON drupal.* TO 'drupal'@'%';"
 mysql -h 127.0.0.1 -P 3306 -u root -e "FLUSH PRIVILEGES;"
-
 
 echo "Install utilities needed for testing"
 mkdir /opt/utils
@@ -38,18 +38,10 @@ if [ -z "${DRUPAL_VERSION:-}" ]; then
    exit 1
 fi
 
-pushd /opt
-composer create-project drupal/recommended-project:$DRUPAL_VERSION drupal
-pushd drupal
-if [ -z "${COMPOSER_PATH:-}" ]; then
-  composer install
-else
-  php -dmemory_limit=-1 $COMPOSER_PATH install
-fi
-
-
-composer require -W "drupal/core-dev:$DRUPAL_VERSION" || true
-composer require drush/drush
+mkdir -p /opt/drupal
+pushd /opt/drupal
+composer create-project "drupal/recommended-project:$DRUPAL_VERSION" .
+composer require -W "drupal/core-dev:$DRUPAL_VERSION"
 
 echo "Setup Drush"
 sudo ln -s /opt/drupal/vendor/bin/drush /usr/bin/drush
@@ -64,4 +56,4 @@ echo "Drush setup drupal site"
 pushd web
 drush si --db-url=mysql://drupal:drupal@127.0.0.1:3306/drupal --yes
 drush runserver 127.0.0.1:8282 &
-until timeout 5 curl -s 127.0.0.1:8282 > /dev/null; do sleep 1; done
+until timeout 5 curl -s 127.0.0.1:8282 -o /dev/null; do sleep 1; done
